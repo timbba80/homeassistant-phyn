e538fc295a35b63fd2d4b48607453709ef3141d9
Martin Kase <mail@kasemartin.cz>
2023-10-23 23:20:19 +0200

    Phyn Smart Water Assistant Support

--- custom_components/phyn/__init__.py
+++ custom_components/phyn/__init__.py
@@ -60,20 +60,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     _LOGGER.debug("Phyn homes: %s", homes)
 
-    #try:
-    await client.mqtt.connect()
-    #except:
-    #    raise HaCannotConnect("Unknown MQTT connection failure")
+    hass.data[DOMAIN][entry.entry_id]["devices"] = devices = [
+        PhynDeviceDataUpdateCoordinator(hass, client, home["id"], device["device_id"])
+        for home in homes
+        for device in home["devices"]
+    ]
 
-    coordinator = PhynDataUpdateCoordinator(hass, client)
-    for home in homes:
-        for device in home["devices"]:
-            if device["product_code"] in ["PW1","PP1","PP2"]:
-                coordinator.add_device(home["id"], device["device_id"], device["product_code"])
-    hass.data[DOMAIN][entry.entry_id]["coordinator"] = coordinator
-
-    await coordinator.async_refresh()
-    await coordinator.async_setup()
+    tasks = [device.async_refresh() for device in devices]
+    await asyncio.gather(*tasks)
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
